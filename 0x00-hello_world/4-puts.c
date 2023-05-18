778143804efc06f974365985ea6d521e9ef96ea9
amyzi <ebiojoidris@icloud.com>
2023-05-18 11:46:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Description: This program prints a specific message using puts.
+ * * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
