21cb543d6905d09235e65943e8335ba016e474a8
amyzi <ebiojoidris@icloud.com>
2023-05-18 12:24:12 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * *main - Entry point
+ *
+ * Description: This program prints an output using printf.
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
