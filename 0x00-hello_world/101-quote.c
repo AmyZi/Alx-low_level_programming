2a3fc8cb5df46ac28010f3c236c1d70159a82303
amyzi <ebiojoidris@icloud.com>
2023-05-19 03:58:06 -0700

    commiting two files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - Entry point
+ *Description: standard error
+ * Return: Return 1 (standard error)
+ */
+
+int main(void)
+{
+	write(2,
+			"and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", 59);
+	return (1);
+}
+
