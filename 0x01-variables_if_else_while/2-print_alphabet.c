7fd10d80381566ee65d1ea6d88b766ec46fcd473
amyzi <ebiojoidris@icloud.com>
2023-05-19 17:22:09 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: Prints with putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'a';
+	
+	while (c <= 'z');
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
