1c1e78ab1bdbdd38288e5f7e2a22e0dc1ca51b50
amyzi <ebiojoidris@icloud.com>
2023-05-23 15:08:58 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+ #include "main.h"
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: Prints alphabets to std output
+*Return: Always 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar('i');
+	}
+	_putchar('\n');
+	return (0);
+}
