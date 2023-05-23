1700cf5896b4582b11d61e435ebb23f06cc7fbe0
amyzi <ebiojoidris@icloud.com>
2023-05-23 15:24:35 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*main - Entry point
+*print_alphabet - Prints alphabets to standard output
 *Description: Prints alphabets to std output
-*Return: Always 0
 */
 void print_alphabet(void)
 {
@@ -14,5 +13,4 @@ void print_alphabet(void)
 		_putchar('i');
 	}
 	_putchar('\n');
-	return (0);
 }
