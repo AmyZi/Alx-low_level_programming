4021b54887bfa50486640258d8a4f91e501f01e3
amyzi <ebiojoidris@icloud.com>
2023-05-24 06:26:12 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,12 +10,12 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = ; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
 				printf("%d, ", i);
 			else if (i == 98)
-				ptintf("%d\n", i);
+				printf("%d\n", i);
 		}
 	}
 	else if (n >= 98)
