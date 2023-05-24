369ec8e4ac72d0c0986a2d33a0f504e86dd480a6
amyzi <ebiojoidris@icloud.com>
2023-05-24 05:18:39 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,19 +14,15 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
-			if (j == 0)
-				_putchar('0');
-			else
+			if (j != 0)
+				_putchar(',');
+			_putchar(' ');
+		}
 			{
 				if (result <= 9)
 					_putchar(' ');
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
-			}
-			if (j != 9)
-			{
-				_putchar(',');
-				_putchar(',');
 			}
 		}
 		_putchar('\n');
