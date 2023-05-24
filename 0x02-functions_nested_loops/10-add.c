1de8e9a09e822cbf0d2fca51b94e34a65beafd45
amyzi <ebiojoidris@icloud.com>
2023-05-24 05:59:29 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *add - adds two integers
+  *@a: int to be added to b
+  *@b: int to be added to a
+  *Return: Result
+  */
+int add(int, int);
+{
+	int sum;
+	sum = a + b;
+	return (sum);
+}
+
