e09efb173fbd136d58fda1f67d456a55c59b12a5
amyzi <ebiojoidris@icloud.com>
2023-05-26 03:42:47 -0700

    10-triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* print_triangle - Prints a triangle pattern of a specific size.
+* @size: The size of the triangle to be printed.
+*
+* Description: This function prints a triangle pattern
+*/
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
