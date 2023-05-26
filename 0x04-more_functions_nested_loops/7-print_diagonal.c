6fd3189cddeca04c18669471e972bd47b8cce627
amyzi <ebiojoidris@icloud.com>
2023-05-26 02:36:00 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_diagonal - Prints a diagonal line of a specific length.
+* @n: The length of the diagonal line to be printed.
+*
+* Description: This function prints a diagonal line consisting
+* of a specified number of '\' characters, representing a diagonal
+* line on the terminal.
+*/
+void print_diagonal(int n)
+{
+	int i;
+	int space;
+
+	for (i = 0; i < n; i++)
+	{
+		for space = 0; space < i; space++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n')
+	}
+	if (n <= 0)
+	{
+		_putchar('\n')
+	}
+}
