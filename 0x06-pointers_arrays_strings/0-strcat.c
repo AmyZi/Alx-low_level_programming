579c02ee6e10ed755d747ffdb13b9d40bad0eecb
amyzi <ebiojoidris@icloud.com>
2023-05-31 22:17:34 -0700

    concat string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,17 +10,21 @@ char *_strcat(char *dest, char *src)
 	int it4dest;
 	int it4src;
 
-	in4dest = 0;
-	while (dest[in4dest] != '\0')
+	it4dest = 0;
+	while (dest[it4dest] != '\0')
 	{
-		in4dest++;
+		it4dest++;
 	}
 	it4src = 0;
 	while (src[it4src] != '\0')
 	{
-		in4dest++;
+		if (src[it4src] != '\n')
+		{
+			dest[it4dest] = src[it4src];
+		it4dest++;
+		}
 		it4src++;
 	}
-	dest[it4dest] != '\0';
+	dest[it4dest] = '\0';
 	return (dest);
 }
