81757fb6c386479317754b2edc8ccc0f785d5c36
amyzi <ebiojoidris@icloud.com>
2023-06-03 00:42:25 -0700

    concat string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,30 +1,27 @@
 #include "main.h"
 /**
-  *_strcat - This function concats two strings and returns the destination
-  *@dest: first string to concat
-  *@src: the other string to concat with
-  *Return: result
-  */
+*_strcat - This function concats two strings and returns the destination
+*@dest: first string to concat
+*@src: the other string to concat with
+*Return: result
+*/
 char *_strcat(char *dest, char *src)
 {
-	int it4dest;
-	int it4src;
+int i;
+int j;
 
-	it4dest = 0;
-	while (dest[it4dest] != '\0')
-	{
-		it4dest++;
-	}
-	it4src = 0;
-	while (src[it4src] != '\0')
-	{
-		if (src[it4src] != '\n')
-		{
-			dest[it4dest] = src[it4src];
-		it4dest++;
-		}
-		it4src++;
-	}
-	dest[it4dest] = '\0';
-	return (dest);
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+  *print_number - this function prints an integer
+  *@n: the integer
+  ^Return: n
+  */
+void print_number(int n)
+{
+
