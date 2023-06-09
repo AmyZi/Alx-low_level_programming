db751300f2c5ccc024d8934d2af091f80e299838
amyzi <ebiojoidris@icloud.com>
2023-06-09 00:10:49 -0700

    square or a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_sqrt_recursion - This function prints natural square of a number
+ *@n: parameter
+ *Return: result
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, i));
+}
+/**
+  *_sqrt - _sqrt_recursion
+  *@n: int param
+  *@i: int param
+  *Return: sqrt
+  */
+int _sqrt(int n, int i)
+{
+	if (n < 0)
+		return (-1);
+	if ((i * i) > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + i));
+}
