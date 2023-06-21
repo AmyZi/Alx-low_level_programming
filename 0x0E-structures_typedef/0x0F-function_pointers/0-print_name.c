313ef91725d8c7cc4c7470a936bd785916a4ebdf
amyzi <ebiojoidris@icloud.com>
2023-06-21 04:05:01 -0700

    print name

---
+++ 0x0E-structures_typedef/0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  *print_name - this functions prints a name
+  *@name: pointer to name
+  *@f: pointer to function
+  *@: pointer to xter
+  *Return: pointer to name
+  */
+void print_name(char *name, void (*f)(char *));
+{
+	if (f != NULL)
+		f(name);
+}
