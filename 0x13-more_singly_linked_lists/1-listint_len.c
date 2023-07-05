8ef78f0c1b3786bcc10b8900481d84665777cf01
amyzi <ebiojoidris@icloud.com>
2023-07-05 09:18:56 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  *listint_len - Calculate the number of elements
+  *@n: pointer to a list
+  *Return: Integer
+  **/
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *tp;
+	unsigned int count = 0;
+
+	tp = h;
+	while (tp)
+	{
+		count++;
+		tp = tp->next;
+	}
+	return (count);
+}
