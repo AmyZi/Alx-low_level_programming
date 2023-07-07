2409f2ff44c84c22803ed642a8d25f9763c6be64
amyzi <ebiojoidris@icloud.com>
2023-07-06 19:34:04 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,26 +1,51 @@
 #include "lists.h"
-/**
-  * print_listint_safe - prints a linked list, safely
-  * @head: list of type listint_t to print
-  * Return: number of nodes in the list
-**/
-size_t print_listint_safe(const listint_t *head)
-{
-	size_t num = 0;
-	long int diff;
+#include <stdlib.h>
 
-	while (head)
-	{
-		diff = head - head->next;
-		num++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
-		}
-	}
-	return (num);
+/**
+* insert_nodeint_at_index - Insert a new node at a given position.
+* @head: Pointer to a pointer to the first node.
+* @idx: Index at which the new node should be inserted.
+* @n: Data of the new node.
+* Return: Address of the new node, or NULL on failure.
+**/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+listint_t *new_node, *temp;
+unsigned int i;
+
+if (head == NULL)
+return (NULL);
+
+if (idx == 0 || *head == NULL)
+{
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
+new_node->next = *head;
+*head = new_node;
+return (new_node);
 }
+
+temp = *head;
+for (i = 0; i < idx - 1; i++)
+{
+if (temp == NULL)
+return (NULL);
+temp = temp->next;
+}
+
+if (temp == NULL)
+return (NULL);
+
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
+
+new_node->n = n;
+new_node->next = temp->next;
+temp->next = new_node;
+
+return (new_node);
+}
+
